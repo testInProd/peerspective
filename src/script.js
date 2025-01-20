4d91f92a6000bdec2a8bc45c06c1fe5d541a4508
Luis Orozco <orozcolui@gmail.com>
2025-01-20 12:08:07 -0600

    center the image and add a black background

--- src/script.js
+++ src/script.js
@@ -2,7 +2,8 @@ const req = new XMLHttpRequest();
 req.open('GET', 'https://epic.gsfc.nasa.gov/api/natural');
 req.onload = function () {
     const data = JSON.parse(this.responseText);
-    const imageId = data[0].image;
+    const randomIndex = Math.floor(Math.random() * data.length);
+    const imageId = data[randomIndex].image;    
 
     const today = new Date(data[0].date);
     const year = today.getFullYear();
@@ -11,7 +12,17 @@ req.onload = function () {
 
     const imageUrl = `https://epic.gsfc.nasa.gov/archive/natural/${year}/${month}/${day}/png/${imageId}.png`;
     document.body.style.backgroundImage = `url(${imageUrl})`;
-    document.body.style.backgroundSize = 'cover';
+    document.body.style.backgroundSize = 'contain';
     document.body.style.backgroundRepeat = 'no-repeat';
+    document.body.style.backgroundPosition = 'center';
+    document.body.style.backgroundColor = 'black';
+    document.body.style.backgroundBlendMode = 'screen';
+    document.documentElement.style.height = '100%';
+    document.documentElement.style.width = '100%';
+    document.body.style.height = '100%';
+    document.body.style.width = '100%';
+    document.body.style.margin = '0';
+    document.body.style.padding = '0';  
+
 };
 req.send();
