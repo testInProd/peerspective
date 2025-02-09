6cb334cc5543f26dac678e347dda4090f2e052df
Luis Orozco <orozcolui@gmail.com>
2025-02-09 09:21:44 -0600

    update main page to pull the nasa picture of the day as a background image

--- src/main-script.js
+++ src/main-script.js
@@ -1,22 +1,26 @@
 const imageId = 'rotating-earth-1.gif';
 
-const backgroundImage = new Image();
-backgroundImage.src = `./assets/${imageId}`;
-backgroundImage.onload = function() {
-    document.body.style.backgroundImage = `url(${backgroundImage.src})`;
-    document.body.style.backgroundSize = 'contain';
-    document.body.style.backgroundRepeat = 'no-repeat';
-    document.body.style.backgroundPosition = 'center';
-    document.body.style.backgroundColor = 'black';
-    document.body.style.backgroundBlendMode = 'screen';
-    document.documentElement.style.height = '100%';
-    document.documentElement.style.width = '100%';
-    document.body.style.height = '100%';
-    document.body.style.width = '100%';
-    document.body.style.margin = '0';
-    document.body.style.padding = '0';  
+const req = new XMLHttpRequest();
+req.open('GET', 'https://api.nasa.gov/planetary/apod?api_key=ylgtDPIaVo0tiEl0ndkEOiD2ePfDEcwrf1QOGPRS');
 
-    const button = document.querySelector('.start-button');
+req.onload = function () {
+  const data = JSON.parse(this.responseText);
+  const imageUrl = data.hdurl;
+
+  document.body.style.backgroundImage = `url(${imageUrl})`;
+  document.body.style.backgroundSize = 'contain';
+  document.body.style.backgroundRepeat = 'no-repeat';
+  document.body.style.backgroundPosition = 'center';
+  document.body.style.backgroundColor = 'black';
+  document.body.style.backgroundBlendMode = 'screen';
+  document.documentElement.style.height = '100%';
+  document.documentElement.style.width = '100%';
+  document.body.style.height = '100%';
+  document.body.style.width = '100%';
+  document.body.style.margin = '0';
+  document.body.style.padding = '0';
+
+  const button = document.querySelector('.start-button');
 
 if (button) {
   button.addEventListener('click', () => {
@@ -27,3 +31,4 @@ if (button) {
 }
 
 };
+req.send();
