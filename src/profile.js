d21734ad0b36a6170cc2576f363b57f9aae95fae
Luis Orozco <orozcolui@gmail.com>
2025-04-02 19:15:26 -0500

    make the profile cards look better

--- src/profile.js
+++ src/profile.js
@@ -1,15 +1,15 @@
-// profile.js
 const profileCards = document.getElementById('profile-cards');
 const storedCards = localStorage.getItem('cards');
 if (storedCards) {
   const cards = JSON.parse(storedCards);
   cards.forEach((card) => {
     const newCard = document.createElement('div');
+    newCard.classList.add('card');
     newCard.innerHTML = `
       <h2>${card.name}</h2>
-      <p>${card.description}</p>
-      <p>${card.price}</p>
       <img src="${card.image}" alt="${card.name}">
+      <p class="description">${card.description}</p>
+      <p class="price">$${card.price}</p>
     `;
     profileCards.appendChild(newCard);
   });
